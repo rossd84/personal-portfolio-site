4109ba82b7ecac098d9d1d769345c36bfcf341e7
Darby Ross <52804447+rossd84@users.noreply.github.com>
2023-10-03 10:25:31 -0500

    info changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,11 @@ const About = () => {
 		>
 			<div className='max-w-[1000px] w-full grid md:grid-cols-2 gap-8'>
 					<div className='text-2xl font-bold'>
-						<p>Hi. I'm Darby, nice to meet you. Please take a look around.</p>
+						<p>Welcome. Nice to meet you.</p>
 					</div>
 					<div>
 						<p>
-							I am passionate about building excellent software that improves the lives of those around me. I specialize
-							in creating software for clients ranging from individuals and small-businesses all the way to large
-							enterprise corporations. What you you do if you had a software expert available at your fingertips?
+							Welcome to my portfolio! I'm Darby, a passionate web developer based in Mississippi with four years of experience in crafting digital solutions that bridge the gap between creativity and functionality. My journey in the world of web development has allowed me to work with a diverse range of clients, from small businesses looking to establish their online presence to large enterprises seeking robust, fullstack applications. I thrive on turning ideas into dynamic, user-friendly websites and applications that not only meet but exceed expectations. With a keen eye for design and a deep understanding of the latest technologies, I'm committed to delivering web solutions that not only look great but also perform flawlessly. Explore my portfolio to see some of my exciting projects and discover how I can help bring your web-based ideas to life.
 						</p>
 					</div>
 				</div>
